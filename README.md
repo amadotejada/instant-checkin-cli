1d3a9f39cf0ced3eddb4edf813af59dbffb360e5
Amado Tejada <44622125+amadotejada@users.noreply.github.com>
2022-07-11 16:13:20 -0400

    initial upload

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
 # instant-checkin-cli
+##### Written by [Amado Tejada](https://www.linkedin.com/in/amadotejada/)
+##
+
+*instant-checkin-cli* is a companion CLI tool for [instant-checkin](https://github.com/amadotejada/instant-checkin) 
+
+*instant-checkin-cli* allows you to force computer to check-in to Jamf MDM via the command line.
+
+<br/>
+ <img src="./screenshots/1080_4.gif" width="100%">
+
+## Requirements
+-  python 3.6+
+- `pip install -r requirements.txt`
+- [unopass](https://github.com/amadotejada/unopass) configured with your API credentials
+- [instant-checkin](https://github.com/amadotejada/instant-checkin) configured
+
+## Jamf API
+*instant-checkin-cli* uses [unopass](https://github.com/amadotejada/unopass) to retrieve your Jamf API credentials at runtime. Set them up in your 1Password account.
+
+Create a Jamf standard user with the minimum permissions to use *instant-checkin-cli*
+
+- Permissions
+    * Computers: Read, Update
+    * Users: Update
+
+Upload your API credentials to 1Password to use it with unopass
+
+<img src="./screenshots/jamfapi.png" width="100%">
+
+## Usage
+
+1. add an alias shortcut to your shell:
+    - E.g `alias ici="python3 instant-checkin-cli.py"`
+
+2. use the shortcut:
+    - `ici`
+
+3. [unopass](https://github.com/amadotejada/unopass) will retrieve your Jamf API credentials
+
+<img src="./screenshots/unopass.png" width="100%">
+
+4. Enter the username of the user you want to check-in
+
+<img src="./screenshots/prompt.png" width="100%">
+
+
+5. A list of computers for that  user will be displayed - Select one
+
+<img src="./screenshots/results.png" width="100%">
+
+6. Profit - Profile will be sent to computer instantly for immediate check-in
+
+<img src="./screenshots/done.png" width="100%">
+
+## Other
+When users are not found in Jamf
+
+<img src="./screenshots/notfound.png" width="100%">
+
+When users are not ready for Instant Check-in
+
+<img src="./screenshots/notready.png" width="100%">
+
+Workflow
+
+<img src="./screenshots/workflow.svg" width="100%">
+
+##
+### License
+
+*instant-checkin-cli* is released under the [MIT License](https://github.com/amadotejada/instant-checkin-cli/blob/main/LICENSE)
---
+++ instant_checkin_cli.py
@@ -0,0 +1,111 @@
+import requests
+from unopass import unopass as secret
+
+try:
+    print("\n\033[93mRetrieving Jamf secrets via unopass...\033[0m")
+    username = secret.unopass("personal", "hyper_jamf", "username")
+    password = secret.unopass("personal", "hyper_jamf", "credential")
+    jss = secret.unopass("personal", "hyper_Jamf", "url")
+except Exception as e:
+    print(f"\n\033[91mError\033[0m: {e}")
+    exit(1)
+
+
+def getBearerToken() -> str:
+    url = f"{jss}/api/v1/auth/token"
+    headers = {"Accept": "application/json"}
+    response = requests.post(url, auth=(username, password), headers=headers)
+    token = response.json()['token']
+    return token
+
+
+def invalidateToken(token: str) -> str:
+    url = f"{jss}/api/v1/auth/invalidate-token"
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.post(url, headers=headers)
+    secret.signout(deauthorize=True)
+
+    if response.status_code == 204:
+        print("\n\033[92mJamf token invalidated OK\033[0m")
+    elif response.status_code == 401:
+        print("\nJamf token already invalidated")
+    else:
+        print("\nError invalidating Jamf token")
+
+
+def banner():
+    print("\n\033[92mWelcome to the instant-checkin CLI\033[0m\n\n-------------------------------------")
+
+
+def user_input() -> str:
+    print("\nEnter the employee username: ")
+    username = input()
+    return username
+
+
+def update_room(chosen: str, token: str) -> None:
+    name = (chosen.split(":")[1])
+    ids = (chosen.split(":")[0])
+    headers = {"Authorization": f"Bearer {token}"}
+    data = "<computer><location><room>check-in</room></location></computer>"
+    url = f"{jss}/JSSResource/computers/id/{ids}"
+    resp = requests.put(url, data=data, headers=headers)
+
+    if resp.status_code == 201:
+        print(f"\n\033[92mSuccess\033[0m: {name} will be check-in shortly.")
+    else:
+        print(f"\n\033[91mError\033[0m: {name} was not updated.")
+
+    invalidateToken(token)
+
+
+def main() -> None:
+    banner()
+    computers = []
+    token = getBearerToken()
+    username = user_input()
+    url = f"{jss}/JSSResource/computers/match/{username}"
+    headers = {"Accept": "application/json", "Authorization": f"Bearer {token}"}
+    response = requests.get(url, headers=headers).json()
+    data = response['computers']
+
+    if data:
+        for computer in data:
+            if "ready" in computer['room'].lower():
+                computers.append(f"{computer['id']}:{computer['name']}")
+            else:
+                print(f"\n\033[91mWarning\033[0m: {computer['name']}: Not ready for Instant Check-in")
+                print(f"\nDeploy this policy to get the user's computer ready:\n{jss}/policies.html?id=157")
+    else:
+        print(f"\n\033[91mError\033[0m: No computers found for {username}\n")
+        invalidateToken(token)
+        exit(1)
+
+    if computers:
+        for ids, name in enumerate(computers, 1):
+            print("\n-------------------------------------")
+            print(f"\n\033[92mJamf Computer Lookup Results:\033[0m {username}\n\n{ids}, Computer {name}")
+
+        try:
+            ids = int(input("\nEnter a computer number from the list : "))
+        except ValueError:
+            print("You fail at typing numbers.")
+            return
+
+        try:
+            chosen = computers[ids-1]
+            print("\n-------------------------------------")
+            print(f"\n\033[93mRequesting an instant check-in\033[0m: {chosen}")
+            update_room(chosen, token)
+        except IndexError:
+            print("Try a number in range next time.")
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\n\n\033[91mExiting...\033[0m")
+        invalidateToken(getBearerToken())
+        secret.signout(deauthorize=True)
+        exit()
