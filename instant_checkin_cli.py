95b8ea4e8162aa3bc68e9eb14191506c7bf92c0a
Amado Tejada <44622125+amadotejada@users.noreply.github.com>
2022-07-13 03:20:18 -0400

    usernames, enumerate fixes & more

--- instant_checkin_cli.py
+++ instant_checkin_cli.py
@@ -1,6 +1,7 @@
 import requests
 from unopass import unopass as secret
 
+
 try:
     print("\n\033[93mRetrieving Jamf secrets via unopass...\033[0m")
     username = secret.unopass("personal", "hyper_jamf", "username")
@@ -23,7 +24,6 @@ def invalidateToken(token: str) -> str:
     url = f"{jss}/api/v1/auth/invalidate-token"
     headers = {"Authorization": f"Bearer {token}"}
     response = requests.post(url, headers=headers)
-    secret.signout(deauthorize=True)
 
     if response.status_code == 204:
         print("\n\033[92mJamf token invalidated OK\033[0m")
@@ -34,13 +34,22 @@ def invalidateToken(token: str) -> str:
 
 
 def banner():
-    print("\n\033[92mWelcome to the instant-checkin CLI\033[0m\n\n-------------------------------------")
+    print(
+        "\n\033[92mWelcome to the instant-checkin CLI"
+        "\033[0m\n\n-------------------------------------"
+    )
 
 
 def user_input() -> str:
-    print("\nEnter the employee username: ")
-    username = input()
-    return username
+    while True:
+        print("\nEnter the employee username: Format amado.tejada\n")
+        username = input()
+        if "." in username:
+            return username
+        else:
+            print(
+                f"\n\033[91mError\033[0m: {username} is invalid | Format amado.tejada"
+            )
 
 
 def update_room(chosen: str, token: str) -> None:
@@ -51,54 +60,94 @@ def update_room(chosen: str, token: str) -> None:
     url = f"{jss}/JSSResource/computers/id/{ids}"
     resp = requests.put(url, data=data, headers=headers)
 
-    if resp.status_code == 201:
-        print(f"\n\033[92mSuccess\033[0m: {name} will be check-in shortly.")
-    else:
-        print(f"\n\033[91mError\033[0m: {name} was not updated.")
-
-    invalidateToken(token)
+    try:
+        if resp.status_code == 201:
+            print(f"\n\033[92mSuccess\033[0m: {ids}:{name} 🎉")
+        elif resp.status_code == 409:
+            print(f"\n\033[93mSkipping\033[0m: Jamf duplicate serial# for {ids}:{name}")
+        else:
+            print(resp.status_code)
+            print(f"\n\033[91mError\033[0m: {ids}:{name} was not updated")
+    except Exception as e:
+        print(f"\n\033[91mError\033[0m: {e}")
 
 
 def main() -> None:
     banner()
-    computers = []
     token = getBearerToken()
     username = user_input()
-    url = f"{jss}/JSSResource/computers/match/{username}"
-    headers = {"Accept": "application/json", "Authorization": f"Bearer {token}"}
-    response = requests.get(url, headers=headers).json()
-    data = response["computers"]
+    computers, usernames = [], []
+    results, used = [], []
+    usernames = set()
+    usernames.add(username)
+    usernames.add(username.replace(".", ""))
+    usernames.add(username + "@DOMAIN.com")  # change to Jamf domain
+    usernames.add(username.replace(".", "") + "@DOMAIN.com")
 
-    if data:
-        for computer in data:
-            if "ready" in computer["room"].lower():
-                computers.append(f"{computer['id']}:{computer['name']}")
+    for u in usernames:
+        headers = {"Accept": "application/json", "Authorization": f"Bearer {token}"}
+        url = f"{jss}/JSSResource/computers/match/{u}"
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        if data != []:
+            for u in data["computers"]:
+                computers.append(u)
+
+    unique = [x for x in computers if x not in used and (used.append(x) or True)]
+    for c in unique:
+        if "ready" in c["room"].lower():
+            results.append(f"{c['id']}:{c['name']}")
+        else:
+            print(
+                f"\n\033[91mWarning: Not ready for Instant Check-in - deploy policy\033[0m "
+                f"\nID: {c['id']}, Laptop: {c['name']}, Serial: {c['serial_number']} "
+                f"\nPolicy: {jss}/policies.html?id=157\nLaptop: {jss}/computers.html?id={c['id']} 💻"
+            )
+
+    if results:
+        sresults = set(results)
+        print(
+            f"\n-------------------------------------\n"
+            f"\n\033[92mJamf Laptop Lookup Results ({len(sresults)}):\033[0m {username}\n"
+        )
+
+        for ids, name in enumerate(sorted(sresults), start=1):
+            print(f"{ids}, Laptop {name}")
+
+        sorted_results = sorted(sresults)
+
+        if len(sorted_results) > 1:
+            print(
+                "\nEnter a number from above or type "
+                "\033[92mALL\033[0m to check-in all laptops:\n"
+            )
+        else:
+            print(
+                "\nEnter the number from above to check-in the laptop:\n"
+            )
+
+        while True:
+            chosen = input()
+            if chosen.upper() == "ALL":
+                for c in sorted_results:
+                    update_room(c, token)
+                break
+            elif chosen.isdigit():
+                if int(chosen) <= len(sorted_results) and int(chosen) > 0:
+                    update_room(sorted_results[int(chosen) - 1], token)
+                    break
+                else:
+                    print(
+                        f"\n\033[91mError\033[0m: {chosen} is not a valid option, try again below\n"
+                    )
             else:
-                print(f"\n\033[91mWarning\033[0m: {computer['name']}: Not ready for Instant Check-in")
-                print(f"\nDeploy this policy to get the user's computer ready:\n{jss}/policies.html?id=157")
+                print(
+                    f"\n\033[91mError\033[0m: {chosen} is not a valid option, try again below\n"
+                )
     else:
-        print(f"\n\033[91mError\033[0m: No computers found for {username}\n")
+        print(f"\n\033[91mError\033[0m: No ready computers found for {username}")
         invalidateToken(token)
         exit(1)
-
-    if computers:
-        for ids, name in enumerate(computers, 1):
-            print("\n-------------------------------------")
-            print(f"\n\033[92mJamf Computer Lookup Results:\033[0m {username}\n\n{ids}, Computer {name}")
-
-        try:
-            ids = int(input("\nEnter a computer number from the list : "))
-        except ValueError:
-            print("You fail at typing numbers.")
-            return
-
-        try:
-            chosen = computers[ids - 1]
-            print("\n-------------------------------------")
-            print(f"\n\033[93mRequesting an instant check-in\033[0m: {chosen}")
-            update_room(chosen, token)
-        except IndexError:
-            print("Try a number in range next time.")
 
 
 if __name__ == "__main__":
@@ -106,9 +155,10 @@ if __name__ == "__main__":
         main()
     except Exception as e:
         print(f"\n\033[91mError\033[0m: {e}")
+        secret.signout(deauthorize=True)
         exit(1)
     except KeyboardInterrupt:
         print("\n\n\033[91mExiting...\033[0m")
         invalidateToken(getBearerToken())
         secret.signout(deauthorize=True)
-        exit()
+        exit(1)
